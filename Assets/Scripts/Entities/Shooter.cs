3915c0d85a43998e12f76544cd28f78723529a1e
German Lungo <g.lungo.m@gmail.com>
2022-02-03 20:34:03 -0300

    Bullet Shooting System

---
+++ Assets/Scripts/Entities/Shooter.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace Entities
+{
+    [GenerateAuthoringComponent]
+    public struct Shooter : IComponentData
+    {
+        public float FireCooldown;
+        public Entity BulletPrefab;
+    }
+}
---
+++ Assets/Scripts/Systems/BulletSpawnerSystem.cs
@@ -0,0 +1,35 @@
+﻿using Entities;
+using Physics;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Systems
+{
+    public class BulletSpawnerSystem : SystemBase
+    {
+        private float _elapsedTime;
+        private CustomInput _input;
+
+        protected override void OnCreate()
+        {
+            //TODO: is there a better method to handle the new input system?
+            _input = new CustomInput();
+            _input.Keyboard.Enable();
+            RequireSingletonForUpdate<Shooter>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var shooterEntity = GetSingletonEntity<Shooter>();
+            var shooterData = EntityManager.GetComponentData<Shooter>(shooterEntity);
+            
+            _elapsedTime += Time.DeltaTime;
+            if (!_input.Keyboard.ShipSkills.triggered || _elapsedTime < shooterData.FireCooldown) return;
+            
+            _elapsedTime = 0;
+            var bullet = EntityManager.Instantiate(shooterData.BulletPrefab);
+            EntityManager.SetComponentData(bullet, EntityManager.GetComponentData<Rotation>(shooterEntity));
+            EntityManager.SetComponentData(bullet, EntityManager.GetComponentData<Translation>(shooterEntity));
+        }
+    }
+}
