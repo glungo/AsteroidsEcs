baa35209882d64bc082a7788cf03743a47d23ee8
German Lungo <g.lungo.m@gmail.com>
2022-02-07 15:15:36 -0300

    Enemy Aiming System

--- Assets/Scripts/Components/Ufo.cs
+++ Assets/Scripts/Components/Ufo.cs
@@ -6,6 +6,7 @@ namespace Components
     [GenerateAuthoringComponent]
     public struct Ufo : IComponentData
     {
+        public float ElapsedTimeSinceLastShot;
         public float FireCooldown;
         public Entity BulletPrefab;
     }
---
+++ Assets/Scripts/Systems/UfoAimingSystem.cs
@@ -0,0 +1,43 @@
+﻿using Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Systems
+{
+    public class UfoAimingSystem : SystemBase
+    {
+        private EndSimulationEntityCommandBufferSystem _endSimulationEcbSystem;
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            _endSimulationEcbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            RequireSingletonForUpdate<Player>();
+        }
+        
+        protected override void OnUpdate()
+        {
+            var playerPosition = EntityManager.GetComponentData<Translation>(GetSingletonEntity<Player>()).Value;
+            var ecb = _endSimulationEcbSystem.CreateCommandBuffer().AsParallelWriter();
+            var deltaTime = Time.DeltaTime;
+            Entities.ForEach((Entity entity, int entityInQueryIndex, ref Rotation rot, ref Translation translation, ref Ufo ufo, in LocalToWorld ltw) =>
+            {
+                //shoot player
+                ufo.ElapsedTimeSinceLastShot += deltaTime;
+                if (ufo.ElapsedTimeSinceLastShot < ufo.FireCooldown) return;
+                
+                var dir = playerPosition - translation.Value;
+                Debug.DrawRay(translation.Value, dir);
+                var bullet = ecb.Instantiate(entityInQueryIndex, ufo.BulletPrefab);
+                ecb.SetComponent(entityInQueryIndex, bullet, new Rotation
+                {
+                    Value = quaternion.LookRotation(ltw.Forward,math.normalize(dir))
+                });
+                ecb.SetComponent(entityInQueryIndex, bullet, translation);
+                ufo.ElapsedTimeSinceLastShot = 0;
+
+            }).ScheduleParallel();
+        }
+    }
+}
