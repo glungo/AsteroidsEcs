6043fc58b4aad416353141dc963fc9df3c4162c4
German Lungo <g.lungo.m@gmail.com>
2022-02-08 01:40:14 -0300

    power up definitions

---
+++ Assets/Scripts/Components/MultiBulletPowerUp.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace Components
+{
+    [GenerateAuthoringComponent]
+    public struct MultiBulletPowerUp : IComponentData
+    {
+        public float Duration;
+        public float RunningTime;
+    }
+}
---
+++ Assets/Scripts/Components/ShieldPowerUp.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace Components
+{
+    [GenerateAuthoringComponent]
+    public struct ShieldPowerUp : IComponentData
+    {
+        public float Duration;
+        public float RunningTime;
+    }
+}
